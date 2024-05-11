16f41837bc312a8f1a630cc34c1f074f16494d47
Timothy Cherney <tjcherney@hotmail.com>
2024-05-10 22:52:25 -0700

    started kirby project

---
+++ src/main.ts
@@ -0,0 +1,31 @@
+import {k} from "./kaboomCtx";
+
+async function gameSetup() {
+    k.loadSprite("assets", "./kirby-like.png", {
+        sliceX: 9,
+        sliceY: 10,
+        anims: {
+            kirbIdle: 0,
+            kirbInhaling: 1,
+            kirbFull: 2,
+            kirbInhaleEffect: { from: 3, to: 8, speed: 15, loop: true},
+            shootingStar: 9,
+            flame: {from: 36, to: 37, speed: 4, loop: true},
+            guyIdle: 18,
+            guyWalk: {from: 18, to: 19, speed: 4, loop: true},
+            bird: {from: 27, to: 28, speed: 4, loop: true},
+        },
+    });
+
+    k.loadSprite("level-1", "./level1.png");
+    k.scene("level-1", () =>{
+        k.setGravity(2100);
+        k.add([
+
+        ]);
+    });
+    
+    k.go("level-1");
+}
+
+gameSetup();
