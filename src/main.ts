8f575e2c447bae24d3e20e3265422311b08b907f
Timothy Cherney <tjcherney@hotmail.com>
2024-06-10 02:51:25 -0700

    finished kirby tut

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
-import {k} from "./kaboomCtx";
+import { makeBirdEnemy, makeFlameEnemy, makeGuyEnemy, makePlayer, setControls } from "./entities";
+import { k } from "./kaboomCtx";
+import { makeMap } from "./utils";
 
 async function gameSetup() {
     k.loadSprite("assets", "./kirby-like.png", {
@@ -8,23 +10,62 @@ async function gameSetup() {
             kirbIdle: 0,
             kirbInhaling: 1,
             kirbFull: 2,
-            kirbInhaleEffect: { from: 3, to: 8, speed: 15, loop: true},
+            kirbInhaleEffect: { from: 3, to: 8, speed: 15, loop: true },
             shootingStar: 9,
-            flame: {from: 36, to: 37, speed: 4, loop: true},
+            flame: { from: 36, to: 37, speed: 4, loop: true },
             guyIdle: 18,
-            guyWalk: {from: 18, to: 19, speed: 4, loop: true},
-            bird: {from: 27, to: 28, speed: 4, loop: true},
+            guyWalk: { from: 18, to: 19, speed: 4, loop: true },
+            bird: { from: 27, to: 28, speed: 4, loop: true },
         },
     });
+    k.add([k.rect(k.width(), k.height()), k.color(0, 0, 0), k.fixed()]);
+    k.loadSprite("level-1", "./level-1.png");
 
-    k.loadSprite("level-1", "./level1.png");
-    k.scene("level-1", () =>{
+    const { map: level1Layout, spawnPoints: level1SpawnPoints } = await makeMap(k, "level-1");
+
+    k.scene("level-1", () => {
         k.setGravity(2100);
         k.add([
-
+            k.rect(k.width(), k.height()),
+            k.color(k.Color.fromHex("#f7d7db")),
+            k.fixed(),
         ]);
+        k.add(level1Layout);
+        const kirb = makePlayer(
+            k,
+            level1SpawnPoints.player[0].x,
+            level1SpawnPoints.player[0].y
+        )
+        setControls(k, kirb);
+        k.add(kirb);
+        k.camScale(k.vec2(0.7));
+        k.onUpdate(() => {
+            if (kirb.pos.x < level1Layout.pos.x + 432) {
+                k.camPos(kirb.pos.x + 500, 870);
+            }
+        });
+
+        for (const flame of level1SpawnPoints.flame) {
+            makeFlameEnemy(k, flame.x, flame.y);
+        }
+
+        for (const guy of level1SpawnPoints.guy) {
+            makeGuyEnemy(k, guy.x, guy.y);
+        }
+
+        for (const bird of level1SpawnPoints.bird) {
+            const possibleSpeeds = [100, 200, 300];
+            k.loop(10, () => {
+                makeBirdEnemy(
+                    k,
+                    bird.x,
+                    bird.y,
+                    possibleSpeeds[Math.floor(Math.random() * possibleSpeeds.length)]
+                );
+            });
+        }
     });
-    
+
     k.go("level-1");
 }
 
